dd3e5224f2a9bacf761b96881062a00b984b13a8
Palak Ramani <palakramani2000@gmail.com>
2026-05-22 14:52:50 +0530

    issue resolved

--- backend/backend_server/bin/check_db.dart
+++ backend/backend_server/bin/check_db.dart
@@ -3,7 +3,7 @@ import 'package:postgres/postgres.dart';
 void main() async {
   final connection = await Connection.open(
     Endpoint(
-      host: 'dpg-d72h8g4r85hc73dpvam0-a.oregon-postgres.render.com',
+      host: 'dpg-d72h8g4r85hc73dpvam0.oregon-postgres.render.com',
       database: 'expense_tracker_db_sxin',
       username: 'expense_tracker_db_sxin_user',
       password: '9G3cVlXA8D3UAIv7JiMHokjqtg0V7a5l',
